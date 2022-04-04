5477fda2632170fe99784aa10c5b2e473468008d
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2022-04-04 19:04:05 +0530

    implemented splash screen

--- android/app/src/main/java/com/kurmishadi/MainActivity.java
+++ android/app/src/main/java/com/kurmishadi/MainActivity.java
@@ -1,10 +1,19 @@
 package com.kurmishadi;
-
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
+
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
