36ab844df0174c1e45bd351f595b79250d0377a8
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2022-04-09 11:17:27 +0530

    working on localization

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -8,41 +8,70 @@ import AdvanceSearch from '../scenes/search/AdvanceSearch';
 import NewsFeed from '../scenes/home/NewsFeed';
 import Sampark from '../scenes/search/Sampark';
 import ParivarikParichay from '../scenes/search/ParivarikParichay';
+import Login from '../scenes/auth/Login';
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="परिवारिक परिचय" component={ParivarikParichay} options={{
-              headerTitleAlign: 'center',
-              headerTintColor: 'white',
-              headerStyle: {
-                backgroundColor: '#DC1C28',
-              },
-            }} />
-      <Stack.Screen name="संपर्क जानकारी" component={Sampark} options={{
-              headerTitleAlign: 'center',
-              headerTintColor: 'white',
-              headerStyle: {
-                backgroundColor: '#DC1C28',
-              },
-            }} />
-      <Stack.Screen name="Advance Search" component={AdvanceSearch} options={{
-              headerTitleAlign: 'center',
-              headerTintColor: 'white',
-              headerStyle: {
-                backgroundColor: '#DC1C28',
-              },
-            }} />
-      <Stack.Screen name="NewsFeed" component={NewsFeed} options={{
-              headerShown: false,
-            }} />
-            <Stack.Screen name="ContactUs" component={ContactUs} options={{
+      <Stack.Navigator initialRouteName='परिवारिक परिचय'>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          headershow
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="NewsFeed"
+          component={NewsFeed}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="परिवारिक परिचय"
+          component={ParivarikParichay}
+          options={{
+            headerTitleAlign: 'center',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: '#DC1C28',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="संपर्क जानकारी"
+          component={Sampark}
+          options={{
+            headerTitleAlign: 'center',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: '#DC1C28',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Advance Search"
+          component={AdvanceSearch}
+          options={{
+            headerTitleAlign: 'center',
+            headerTintColor: 'white',
+            headerStyle: {
+              backgroundColor: '#DC1C28',
+            },
+          }}
+        />
 
-              headerShown: false,
-            }} />
-           
+        <Stack.Screen
+          name="ContactUs"
+          component={ContactUs}
+          options={{
+            headerShown: false,
+          }}
+        />
+
         <Stack.Screen name="Home" component={HomeComponent} />
       </Stack.Navigator>
     </NavigationContainer>
