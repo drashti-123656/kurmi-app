e64b4cda8a11cc327a7943ad7920f3f7f5b3023f
drashti-123656 <drashti-rnative-intern@resiliencesoft.com>
2022-04-18 11:16:51 +0530

    Dharmikjankari Screen

---
+++ src/utils/schema/dharmikJankariSchema.js
@@ -0,0 +1,11 @@
+import * as Yup from 'yup';
+
+export const DharmikjankarivalidationSchema = Yup.object({
+    caste: Yup.string().required('*Required'),
+    native: Yup.string().required('*Required'),
+    birthtime: Yup.string().required('*Required'),
+    birthplace: Yup.string().required('*Required'),
+    zodiacsign: Yup.string().required('*Required'),
+    auspicious: Yup.string().required('*Required'),
+    bloodgroup: Yup.string().required('*Required'),
+  })
