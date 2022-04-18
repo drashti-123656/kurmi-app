5688fb5b247239cf7dc0384a1ecac1905b818854
drashti-123656 <drashti-rnative-intern@resiliencesoft.com>
2022-04-18 13:39:31 +0530

    personal Information

---
+++ src/utils/schema/personalInformationSchema.js
@@ -0,0 +1,11 @@
+import * as Yup from 'yup';
+
+export const PersonalinformationSchema = Yup.object({
+  height: Yup.string().required('*Required'),
+  maritalstatus: Yup.string().required('*Required'),
+  knowledge: Yup.string().required('*Required'),
+  job: Yup.string().required('*Required'),
+  colour: Yup.string().required('*Required'),
+  disability: Yup.string().required('*Required'),
+  bloodgroup: Yup.string().required('*Required'),
+});
