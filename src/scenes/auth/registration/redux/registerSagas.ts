9e5dfadb89b8061f22b30d18723fcf1ac824e02e
Sanyamis22 <sanya-intern@resiliencesoft.com>
2022-04-21 23:54:02 +0530

    Merge branch 'Dev' of https://github.com/prateek-resiliencesoft/kurmishadiapp into feature/new-contactUs-screen

---
+++ src/scenes/auth/registration/redux/registerSagas.ts
@@ -0,0 +1,23 @@
+import {call} from 'redux-saga/effects';
+import apiClient from './../../../../services/httpServices';
+import {showMessage, hideMessage} from 'react-native-flash-message';
+import {API_URL} from '../../../../services/webConstants';
+import {registrationsFail, registrationSuccess} from './registrationReducer';
+
+export function* registerUser(action) {
+  const payload = action.payload;
+  const response = yield call(apiClient.post, API_URL.REGISTER_USER, payload);
+  if (response.ok) {
+    showMessage({
+      message: 'successfully registered',
+      type: 'success',
+    });
+    registrationSuccess({});
+  } else {
+    showMessage({
+      message: 'Ops, something went wrong',
+      type: 'danger',
+    });
+    registrationsFail(response.problem);
+  }
+}
