83f8bd0292580ed3e08f3fb0f0e879123d6dff31
drashti-123656 <drashti-rnative-intern@resiliencesoft.com>
2022-05-09 17:46:31 +0530

    Remove console

--- src/scenes/passwordChange/redux/passwordSaga.ts
+++ src/scenes/passwordChange/redux/passwordSaga.ts
@@ -1,16 +1,12 @@
 import {call, put} from 'redux-saga/effects';
 import apiClient from './../../../services/httpServices';
 import {API_URL} from '../../../services/webConstants';
-import {navigate} from '../../../navigation/RootNavigation';
 import {showMessage} from 'react-native-flash-message';
-import {Alert} from 'react-native';
 import {changepassword} from './changepasswordReducer';
 
 export function* changePassword(action) {
   const payload = action.payload;
-  console.log('payloaddddd=====>', payload);
   const response = yield call(apiClient.post, API_URL.CHANGE_PASSWORD, payload);
-  console.log('password =====>', response.data);
   if (response.ok) {
     yield put(changepassword(payload));
 
