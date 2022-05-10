b52dea6ae27df37245f21eff7be7d1244a30efed
drashti-123656 <drashti-rnative-intern@resiliencesoft.com>
2022-05-10 12:15:20 +0530

    drawer items in section wise

--- src/navigation/DrawerNavigation.tsx
+++ src/navigation/DrawerNavigation.tsx
@@ -6,12 +6,7 @@ import translate from '../translations/configTranslations';
 import CustomDrawer from './customDrawer';
 import AdvanceSearch from '../scenes/search/AdvanceSearch';
 import Like from '../DrawerNavigationScreen/Like';
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import EStyleSheet from 'react-native-extended-stylesheet';
-import Profile from '../DrawerNavigationScreen/Profile'
-import Widowedprofiles from '../DrawerNavigationScreen/WidowedProfile'
+import Profile from '../DrawerNavigationScreen/Profile';
 import ViewBy from '../DrawerNavigationScreen/ViewBy';
 import ShortLists from '../DrawerNavigationScreen/ShortLists';
 import Sharedbiodata from '../DrawerNavigationScreen/SharedBiodata';
@@ -30,128 +25,56 @@ const DrawerNavigation = () => {
       drawerContent={props => <CustomDrawer {...props} />}>
       <Drawer.Screen
         name={translate('drawerScreen.Home')}
-
-        options={{headerShown: false, drawerIcon:()=>(
-          <Ionicons name="home"size={22} color={styles.color}/>
-        )
-        }}
+        options={{headerShown: false}}
         component={DashboardNavigation}
       />
-      <Drawer.Screen
-        name={translate('drawerScreen.Likes')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="heart" size={22} color={styles.color} />
-          ),
-        }}
-        component={Like}
-      />
+      <Drawer.Screen name={translate('drawerScreen.Likes')} component={Like} />
       <Drawer.Screen
         name={translate('drawerScreen.View By')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="eye" size={22} color={styles.color} />
-          ),
-        }}
         component={ViewBy}
       />
       <Drawer.Screen
         name={translate('drawerScreen.shortlist')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="star" size={22} color={styles.color} />
-          ),
-        }}
         component={ShortLists}
       />
-       
+
       <Drawer.Screen
         name={translate(
           'drawerScreen.advanced search-property,goverment job,business,place,kurmiraj',
         )}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="search" size={22} color={styles.color} />
-          ),
-        }}
         component={AdvanceSearch}
       />
       <Drawer.Screen
         name={translate('drawerScreen.biodata share')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="document-text" size={22} color={styles.color} />
-          ),
-        }}
         component={Sharedbiodata}
       />
       <Drawer.Screen
         name={translate('drawerScreen.Membership')}
-        options={{
-          drawerIcon: () => (
-            <FontAwesome
-              style={{marginHorizontal: 5}}
-              name="rupee"
-              size={22}
-              color={styles.color}
-            />
-          ),
-        }}
         component={Membershipplan}
       />
 
       <Drawer.Screen
         name={translate('drawerScreen.30+ profile')}
-        options={{
-          drawerIcon: () => (
-            <FontAwesome name="calendar-o" size={22}color={styles.color} />
-          ),
-        }}
         component={Profile}
       />
       <Drawer.Screen
         name={translate('drawerScreen.divorced profile')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="person" size={22}color={styles.color} />
-          ),
-        }}
         component={Divorcedprofile}
       />
       <Drawer.Screen
         name={translate('drawerScreen.Disabled profile')}
-        options={{
-          drawerIcon: () => (
-            <Fontisto name="paralysis-disability" size={22} color={styles.color} />
-          ),
-        }}
         component={DisabilityProfiles}
       />
       <Drawer.Screen
         name={translate('drawerScreen.Need partner')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="person" size={22} color={styles.color} />
-          ),
-        }}
         component={Needpartners}
       />
       <Drawer.Screen
         name={translate('drawerScreen.widowed profile')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="person" size={22} color={styles.color} />
-          ),
-        }}
         component={WidowedProfile}
       />
       <Drawer.Screen
         name={translate('drawerScreen.settings')}
-        options={{
-          drawerIcon: () => (
-            <Ionicons name="settings" size={22} color={styles.color} />
-          ),
-        }}
         component={Settings}
       />
     </Drawer.Navigator>
@@ -159,7 +82,3 @@ const DrawerNavigation = () => {
 };
 
 export default DrawerNavigation;
-const styles = EStyleSheet.create({
-  color:'#DC1C28'
-});
-
--- src/navigation/customDrawer.js
+++ src/navigation/customDrawer.js
@@ -1,24 +1,20 @@
-import {
-  View,
-  Text,
-  ImageBackground,
-  StyleSheet,
-  Image,
-  Linking,
-} from 'react-native';
+import {View, Text, ImageBackground, Image, Linking} from 'react-native';
 import React from 'react';
-import {
-  DrawerContentScrollView,
-  DrawerItemList,
-} from '@react-navigation/drawer';
+import {DrawerContentScrollView} from '@react-navigation/drawer';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {Drawer} from 'react-native-paper';
+import translate from '../translations/configTranslations';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
 const CustomDrawer = props => {
   return (
     <View style={styles.container}>
       <DrawerContentScrollView
         {...props}
-        contentContainerStyle={{backgroundColor: 'white'}}>
+        contentContainerStyle={styles.containStyle}>
         <View>
           <ImageBackground
             source={require('../assets/user.png')}
@@ -41,20 +37,169 @@ const CustomDrawer = props => {
             </Text>
           </ImageBackground>
         </View>
-        <View style={styles.drawerContainer}>
-          <DrawerItemList {...props} />
-        </View>
+
+        <Drawer.Section style={styles.drawerSection}>
+          <Drawer.Item
+            icon={() => <Ionicons name="home" size={22} color={styles.color} />}
+            label={translate('drawerScreen.Home')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.Home'));
+            }}
+          />
+          <Drawer.Item
+            icon={() => (
+              <Ionicons name="heart" size={22} color={styles.color} />
+            )}
+            label={translate('drawerScreen.Likes')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.Likes'));
+            }}
+          />
+          <Drawer.Item
+            icon={() => <Ionicons name="eye" size={22} color={styles.color} />}
+            label={translate('drawerScreen.View By')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.View By'));
+            }}
+          />
+        </Drawer.Section>
+
+        <Drawer.Section style={styles.drawer}>
+          <Drawer.Item
+            icon={() => <Ionicons name="star" size={22} color={styles.color} />}
+            label={translate('drawerScreen.shortlist')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.shortlist'));
+            }}
+          />
+          <Drawer.Item
+            icon={() => (
+              <Ionicons name="search" size={22} color={styles.color} />
+            )}
+            label={translate(
+              'drawerScreen.advanced search-property,goverment job,business,place,kurmiraj',
+            )}
+            onPress={() => {
+              props.navigation.navigate(
+                translate(
+                  'drawerScreen.advanced search-property,goverment job,business,place,kurmiraj',
+                ),
+              );
+            }}
+          />
+          <Drawer.Item
+            icon={() => (
+              <Ionicons name="document-text" size={22} color={styles.color} />
+            )}
+            label={translate('drawerScreen.biodata share')}
+            onPress={() => {
+              props.navigation.navigate(
+                translate('drawerScreen.biodata share'),
+              );
+            }}
+          />
+        </Drawer.Section>
+
+        <Drawer.Section style={styles.drawer}>
+          <Drawer.Item
+            icon={() => (
+              <FontAwesome
+                style={styles.fontawesome}
+                name="rupee"
+                size={22}
+                color={styles.color}
+              />
+            )}
+            label={translate('drawerScreen.Membership')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.Membership'));
+            }}
+          />
+          <Drawer.Item
+            icon={() => (
+              <FontAwesome name="calendar-o" size={22} color={styles.color} />
+            )}
+            label={translate('drawerScreen.30+ profile')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.30+ profile'));
+            }}
+          />
+          <Drawer.Item
+            icon={() => (
+              <Ionicons name="person" size={22} color={styles.color} />
+            )}
+            label={translate('drawerScreen.divorced profile')}
+            onPress={() => {
+              props.navigation.navigate(
+                translate('drawerScreen.divorced profile'),
+              );
+            }}
+          />
+        </Drawer.Section>
+
+        <Drawer.Section style={styles.drawer}>
+          <Drawer.Item
+            icon={() => (
+              <Fontisto
+                name="paralysis-disability"
+                size={22}
+                color={styles.color}
+              />
+            )}
+            label={translate('drawerScreen.Disabled profile')}
+            onPress={() => {
+              props.navigation.navigate(
+                translate('drawerScreen.Disabled profile'),
+              );
+            }}
+          />
+          <Drawer.Item
+            icon={() => (
+              <Ionicons name="person" size={22} color={styles.color} />
+            )}
+            label={translate('drawerScreen.Need partner')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.Need partner'));
+            }}
+          />
+          <Drawer.Item
+            icon={() => (
+              <Ionicons name="person" size={22} color={styles.color} />
+            )}
+            label={translate('drawerScreen.widowed profile')}
+            onPress={() => {
+              props.navigation.navigate(
+                translate('drawerScreen.widowed profile'),
+              );
+            }}
+          />
+
+          <Drawer.Item
+            icon={() => (
+              <Ionicons name="settings" size={22} color={styles.color} />
+            )}
+            label={translate('drawerScreen.settings')}
+            onPress={() => {
+              props.navigation.navigate(translate('drawerScreen.settings'));
+            }}
+          />
+        </Drawer.Section>
       </DrawerContentScrollView>
-      <View></View>
     </View>
   );
 };
 
 export default CustomDrawer;
 
-const styles = StyleSheet.create({
+const styles = EStyleSheet.create({
   container: {
     flex: 1,
+  },
+  fontawesome: {
+    marginHorizontal: 5,
+  },
+  drawer: {
+    marginTop: 10,
   },
   button: {
     alignItems: 'center',
@@ -64,6 +209,9 @@ const styles = StyleSheet.create({
     marginHorizontal: 150,
     marginTop: 20,
     borderRadius: 30,
+  },
+  containStyle: {
+    backgroundColor: 'white',
   },
   logintext: {
     color: 'white',
@@ -95,5 +243,9 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 50,
     left: 10,
+  },
+  color: '#DC1C28',
+  drawerSection: {
+    marginTop: 40,
   },
 });
