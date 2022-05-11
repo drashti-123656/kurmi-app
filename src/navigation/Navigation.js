5207be417e58085ee9814a26a4886755eb1c99d8
Sanyamis22 <sanya@resiliencesoft.com>
2022-05-11 14:48:54 +0530

    ui-updated

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -16,9 +16,10 @@ import DrawerNavigation from './DrawerNavigation';
 import Registration from '../scenes/auth/registration/Registration';
 import DashboardNavigation from './DashboardNavigation';
 import Login from '../scenes/auth/Login';
-import OthersProfile from '../scenes/othersProfile/OthersProfile';
+
 import EStyleSheet from 'react-native-extended-stylesheet';
 import SeeAllProfile from '../scenes/home/SeeAllProfile';
+import OthersProfile from '../scenes/othersProfile/OthersProfile';
 
 
 const Stack = createNativeStackNavigator();
