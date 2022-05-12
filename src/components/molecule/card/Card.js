f86d1c01c2c842002c2d97f6a38f529a110de2c6
Annnnjali <anjali-intern@resiliencesoft.com>
2022-05-12 12:28:03 +0530

    completed profile-card

--- src/components/molecule/card/Card.js
+++ src/components/molecule/card/Card.js
@@ -1,21 +1,22 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
 const Card = ({navigation, item}) => {
   return (
-    
     <View style={styles.profileContainer}>
         <TouchableOpacity
           onPress={() =>
             navigation.navigate('OthersProfile', {
-              id: item.id,
+              id: item.userId,
             })
           }>
+          <View style={styles.mainContainer}>
           <View style={styles.profileImageContainer}>
             <Image
               style={styles.profileImg}
@@ -25,6 +26,9 @@ const Card = ({navigation, item}) => {
               <Text style={styles.profileText}>
                 {item.userFirstName} {item.userLastName}
               </Text>
+              <Text style={styles.profileIntroText}>
+                Age - {item.userAge}, 
+              </Text>
 
               <Text style={styles.profileIntroText}>
                 {item.userCity.cityName}, {item.userState.name},
@@ -32,197 +36,43 @@ const Card = ({navigation, item}) => {
               <Text style={styles.profileIntroText}>
                 {item.userCountry.countryName}
               </Text>
-              <View
-                style={{
-                  borderBottomColor: 'black',
-                  borderBottomWidth: 1,
-                  paddingTop: 35,
-                }}
-              />
-              <View style={styles.bottomContainer}>
-                <TouchableOpacity style={styles.bottmIcons}>
-                  <Icon
-                    name="star-o"
-                    size={30}
-                    color="black"
-                    style={{paddingLeft: 15}}
-                  />
-                  <Text style={styles.bottomText}> Shortlist </Text>
-                </TouchableOpacity>
-              </View>
+            </View>
+            
+          </View>
+          <View
+              style={styles.line}
+            />
+
+            <View style={styles.bottomContainer}>
+              <TouchableOpacity style={styles.bottmIcons}>
+                <Icon name="star-o" size={22} color="#499A30" style={styles.icon}  />
+                <Text style={styles.bottomText}> Shortlist </Text>
+              </TouchableOpacity>
             </View>
           </View>
         </TouchableOpacity>
       </View>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
 
-const styles = StyleSheet.create({
-  cardContainer: {
-    flex: 0.2,
-    backgroundColor: 'red',
-    marginHorizontal: 15,
-    marginVertical: 10,
-    borderRadius: 20,
-  },
-  container: {
-    alignSelf: 'stretch',
-    height: 52,
-    flexDirection: 'row',
-    backgroundColor: '#DC1C28',
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  imageContainer: {
-    marginTop: 18,
-  },
-  vectorImg: {
-    width: 20,
-    height: 5,
-  },
-  vectorImg_1: {
-    width: 15,
-    height: 5,
-  },
-  pinClipart: {
-    position: 'absolute',
-    top: 15,
-    right: 15,
-  },
-  PinClipartImg: {
-    width: 25,
-    height: 25,
-  },
-  navbarText: {
-    color: '#FFFFFF',
-    fontSize: 25,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    marginHorizontal: 20,
-  },
-  title: {
-    marginTop: 10,
-    marginBottom: 10,
-    textAlign: 'center',
-    fontWeight: '500',
-    fontSize: 22,
-    color: '#ffffff',
-  },
-  bottomText: {
-    marginHorizontal: 30,
-    fontSize: 18,
-    color: '#FFFFFF',
-  },
-  radioButtonContainer: {
-    flexDirection: 'row',
-    marginLeft: 10,
-  },
-  ButtonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 80,
-  },
-  radioButton: {
-    height: 25,
-    width: 25,
-    backgroundColor: 'transparent',
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginHorizontal: 20,
-    marginTop: 20,
-  },
-  radioButtonIcon: {
-    height: 15,
-    width: 15,
-    borderRadius: 9,
-    backgroundColor: 'white',
-  },
-  radioButtonText: {
-    fontSize: 22,
-    color: '#FFFFFF',
-    fontWeight: '400',
-    marginTop: 15,
-  },
-  ageContainer: {
-    flexDirection: 'row',
-  },
-  textInput: {
-    backgroundColor: 'white',
-    marginLeft: 30,
-    marginVertical: 10,
-    flex: 0.44,
-    fontSize: 17,
-    borderRadius: 10,
-    padding: 10,
-    height: hp(7),
-    color: 'black',
-  },
-  submitButton: {
-    backgroundColor: '#DC1C28',
-    height: hp(8),
-    marginHorizontal: 30,
-    borderRadius: 10,
-  },
-  textButton: {
-    textAlign: 'center',
-    fontWeight: '400',
-    marginTop: 15,
-    fontSize: 20,
-    color: 'white',
-  },
-  text: {
-    marginTop: 10,
-    marginBottom: 20,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    fontSize: 18,
-    color: '#FFFFFF',
-  },
-  footerContainer: {
-    backgroundColor: '#EDEDED',
-    flexDirection: 'row',
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-  },
-  titleText: {
-    fontWeight: 'bold',
-    color: 'black',
-    fontSize: 22,
-    marginTop: 10,
-  },
-  titleTextNext: {
-    color: '#666666',
-  },
-  profileContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  profileImageContainer: {
-    height: hp('20'),
-    width: wp('90'),
-    marginTop: 30,
-    paddingLeft: 10,
-    backgroundColor: 'white',
-    marginHorizontal: 20,
-    borderRadius: 15,
-    flexDirection: 'row',
+const styles = EStyleSheet.create({
+  profileImageContainer: { 
+     flexDirection: 'row',
   },
   profileImg: {
-    width: wp('25'),
-    height: hp('13'),
+    width: wp('20'),
+    height: hp('10'),
     borderRadius: 100,
     marginTop: 10,
+    marginLeft: 5,
   },
 
   profileText: {
     fontWeight: 'bold',
     color: 'black',
-    paddingTop: 20,
+    paddingTop: 10,
     textAlign: 'center',
     fontSize: 16,
     alignSelf: 'flex-start',
@@ -238,30 +88,41 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-start',
     paddingLeft: 10,
   },
-  footerContainer: {
-    backgroundColor: 'white',
-  },
-  footerTextseeAll: {
-    color: 'red',
-    fontSize: 20,
-    marginBottom: 20,
-    alignSelf: 'center',
-  },
   footerTextContainer: {
     flexDirection: 'column',
-    //alignSelf : 'flex-start'
+    paddingBottom: 10,
   },
-  bottomContainer : {
-    flexDirection : 'row',
-    paddingTop : 10,
-    alignSelf : 'flex-start'
+  line: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    paddingTop: 5,
+    marginLeft: 10,
+    marginRight: 15,
   },
-  bottmIcons : {
-    flexDirection : 'row',
-    alignSelf : 'flex-start',
-   
+  bottomContainer: {
+    flexDirection: 'row',
+    paddingTop: 5,
+    alignSelf : 'center',
+    marginBottom : 5,
   },
-  bottomText : {
-    color: 'black'
+  bottmIcons: {
+    flexDirection: 'row',
+  },
+  bottomText: {
+    alignSelf: 'flex-end',
+    color: '$PRIMARY',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  mainContainer : {
+    flexDirection : 'column',
+    height: hp('20'),
+    width: wp('90'),
+    paddingLeft: 10,
+    backgroundColor: 'white',
+   marginHorizontal: 20,
+    justifyContent : 'center',
+    marginVertical : 5,
+    borderRadius: 15,
   }
 });
--- src/scenes/divorcedProfile/DivorcedProfile.tsx
+++ src/scenes/divorcedProfile/DivorcedProfile.tsx
@@ -38,19 +38,24 @@ const DivorcedProfile = ({navigation}) => {
       payload,
     });
   });
+
+
   const renderItem = ({item}) => {
     return (
       <Card navigation={navigation} item={item} />
     );
   };
+  
   return (
     <RootScreen scrollable={true}>
+      <View style={styles.container}>
       <FlatList
         data={divorcedData}
         renderItem={renderItem}
         keyExtractor={item => item.id}
         initialNumToRender={10}
       />
+      </View>
     </RootScreen>
   );
 };
@@ -58,208 +63,9 @@ const DivorcedProfile = ({navigation}) => {
 export default DivorcedProfile;
 
 const styles = StyleSheet.create({
-  cardContainer: {
-    flex: 0.2,
-    backgroundColor: 'red',
-    marginHorizontal: 15,
-    marginVertical: 10,
-    borderRadius: 20,
-  },
-  container: {
-    alignSelf: 'stretch',
-    height: 52,
-    flexDirection: 'row',
-    backgroundColor: '#DC1C28',
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  imageContainer: {
-    marginTop: 18,
-  },
-  vectorImg: {
-    width: 20,
-    height: 5,
-  },
-  vectorImg_1: {
-    width: 15,
-    height: 5,
-  },
-  pinClipart: {
-    position: 'absolute',
-    top: 15,
-    right: 15,
-  },
-  PinClipartImg: {
-    width: 25,
-    height: 25,
-  },
-  navbarText: {
-    color: '#FFFFFF',
-    fontSize: 25,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    marginHorizontal: 20,
-  },
-  title: {
-    marginTop: 10,
-    marginBottom: 10,
-    textAlign: 'center',
-    fontWeight: '500',
-    fontSize: 22,
-    color: '#ffffff',
-  },
-  bottomText: {
-    marginHorizontal: 30,
-    fontSize: 18,
-    color: '#FFFFFF',
-  },
-  radioButtonContainer: {
-    flexDirection: 'row',
-    marginLeft: 10,
-  },
-  ButtonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 80,
-  },
-  radioButton: {
-    height: 25,
-    width: 25,
-    backgroundColor: 'transparent',
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginHorizontal: 20,
-    marginTop: 20,
-  },
-  radioButtonIcon: {
-    height: 15,
-    width: 15,
-    borderRadius: 9,
-    backgroundColor: 'white',
-  },
-  radioButtonText: {
-    fontSize: 22,
-    color: '#FFFFFF',
-    fontWeight: '400',
-    marginTop: 15,
-  },
-  ageContainer: {
-    flexDirection: 'row',
-  },
-  textInput: {
-    backgroundColor: 'white',
-    marginLeft: 30,
-    marginVertical: 10,
-    flex: 0.44,
-    fontSize: 17,
-    borderRadius: 10,
-    padding: 10,
-    height: hp(7),
-    color: 'black',
-  },
-  submitButton: {
-    backgroundColor: '#DC1C28',
-    height: hp(8),
-    marginHorizontal: 30,
-    borderRadius: 10,
-  },
-  textButton: {
-    textAlign: 'center',
-    fontWeight: '400',
-    marginTop: 15,
-    fontSize: 20,
-    color: 'white',
-  },
-  text: {
-    marginTop: 10,
-    marginBottom: 20,
-    justifyContent: 'center',
-    alignSelf: 'center',
-    fontSize: 18,
-    color: '#FFFFFF',
-  },
-  footerContainer: {
-    backgroundColor: '#EDEDED',
-    flexDirection: 'row',
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-  },
-  titleText: {
-    fontWeight: 'bold',
-    color: 'black',
-    fontSize: 22,
-    marginTop: 10,
-  },
-  titleTextNext: {
-    color: '#666666',
-  },
-  profileContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  profileImageContainer: {
-    height: hp('20'),
-    width: wp('90'),
-    marginTop: 30,
-    paddingLeft: 10,
-    backgroundColor: 'white',
-    marginHorizontal: 20,
-    borderRadius: 15,
-    flexDirection: 'row',
-  },
-  profileImg: {
-    width: wp('25'),
-    height: hp('13'),
-    borderRadius: 100,
-    marginTop: 10,
-  },
-
-  profileText: {
-    fontWeight: 'bold',
-    color: 'black',
-    paddingTop: 20,
-    textAlign: 'center',
-    fontSize: 16,
-    alignSelf: 'flex-start',
-    paddingLeft: 10,
-  },
-  profileIntroText: {
-    color: '#666666',
-    textAlign: 'center',
-    fontSize: 13,
-
-    textAlign: 'center',
-
-    alignSelf: 'flex-start',
-    paddingLeft: 10,
-  },
-  footerContainer: {
-    backgroundColor: 'white',
-  },
-  footerTextseeAll: {
-    color: 'red',
-    fontSize: 20,
-    marginBottom: 20,
-    alignSelf: 'center',
-  },
-  footerTextContainer: {
-    flexDirection: 'column',
-    //alignSelf : 'flex-start'
-  },
-  bottomContainer : {
-    flexDirection : 'row',
-    paddingTop : 10,
-    alignSelf : 'flex-start'
-  },
-  bottmIcons : {
-    flexDirection : 'row',
-    alignSelf : 'flex-start',
-   
-  },
-  bottomText : {
-    color: 'black'
-  }
+  container : {
+    marginBottom : 20,
+    justifyContent : 'center',
+    marginTop : 20, 
+   },
 });
