d6efeafd1d8bc2ec19b1ef9ba507f01a93cafdf6
drashti-123656 <drashti-rnative-intern@resiliencesoft.com>
2022-06-08 17:03:14 +0530

    Merge branch Dev of https://github.com/prateek-resiliencesoft/kurmishadiapp into feature/husky-setup-new

--- src/scenes/viewBy/redux/ViewBySaga.js
+++ src/scenes/viewBy/redux/ViewBySaga.js
@@ -1,58 +1,57 @@
+import {showMessage} from 'react-native-flash-message';
 import {call, put, select} from 'redux-saga/effects';
 import apiClient from '../../../services/httpServices';
-import {showMessage, hideMessage} from 'react-native-flash-message';
 import {API_URL} from '../../../services/webConstants';
 import {PAGE_SIZE} from '../../../utils/constants/appConstants';
 import {
+  viewBySuccess,
+  fetchViewByUserDataStarted,
   fetchViewByDataSuccess,
   fetchViewByUserDataFail,
-  fetchViewByUserDataStarted,
-  viewBySuccess,
 } from './ViewByReducer';
-
 export function* viewByProfile(action) {
   const payload = action.payload;
-  const {ok, problem, data} = yield call(
+  const {ok, data} = yield call(
     apiClient.post,
     API_URL.VIEW_BY_ID_PROFILE,
     payload,
   );
-  console.log('view priofile====>', data);
 
   if (ok) {
+    showMessage({
+      message: 'Profile Visited!',
+      type: 'success',
+    });
     yield put(viewBySuccess(data));
   }
 }
-
 export function* viewByUsers(action) {
   const payload = action.payload;
-  fetchViewByUserDataStarted({});
-  const {ok, problem, data} = yield call(
-    apiClient.post,
-    API_URL.VIEW_BY_USERS,
-    payload,
-  );
+
+  yield put(fetchViewByUserDataStarted({}));
+
+  const {data, ok} = yield call(apiClient.post, API_URL.VIEW_BY_USERS, payload);
+
   let finalProfileList = [];
   if (payload.page > 1) {
     const {viewByUsersData} = yield select(state => state.viewByProfiles);
     finalProfileList = viewByUsersData.concat(data.data);
   } else {
-    finalProfileList = data.data;
+    finalProfileList = data;
   }
-  console.log('view by userssssss==>>', data);
-
   if (ok) {
     yield put(
       fetchViewByDataSuccess({
         profile: finalProfileList,
+        isPaginationRequired: data.data.length === PAGE_SIZE,
         pageNumber: payload.page,
       }),
     );
   } else {
+    yield put(fetchViewByUserDataFail({}));
     showMessage({
-      message: 'Login Your Account!',
+      message: 'Ops, something went wrong',
       type: 'danger',
     });
-    fetchViewByUserDataFail(problem);
   }
 }
