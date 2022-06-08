b97c0d9299ebbfc7f3dda831494f00463402a2db
Annnnjali <anjali@resiliencesoft.com>
2022-06-08 15:18:13 +0530

    loader-pagination-in-widower-screen

--- src/scenes/widowerProfile/redux/widowerSaga.ts
+++ src/scenes/widowerProfile/redux/widowerSaga.ts
@@ -1,7 +1,8 @@
 import {showMessage} from 'react-native-flash-message';
-import {call, put} from 'redux-saga/effects';
+import {call, put, select} from 'redux-saga/effects';
 import apiClient from '../../../services/httpServices';
 import {API_URL} from '../../../services/webConstants';
+import {PAGE_SIZE} from '../../../utils/constants/appConstants';
 import {
   fetchWidowerDataFail,
   fetchWidowerDataStarted,
@@ -13,19 +14,29 @@ export function* widowerStatus(action) {
 
   yield put(fetchWidowerDataStarted({}));
 
-  const {data, ok, problem} = yield call(
-    apiClient.post,
-    API_URL.WIDOWER_DATA,
-    payload,
-  );
+  const {data, ok} = yield call(apiClient.post, API_URL.WIDOWER_DATA, payload);
+
+  let finalProfileList = [];
+  if (payload.page > 1) {
+    const {widowerData} = yield select(state => state.widowerProfile);
+    finalProfileList = widowerData.concat(data.data);
+  } else {
+    finalProfileList = data.data;
+  }
 
   if (ok) {
-    yield put(fetchWidowerDataSuccess(data.data));
+    yield put(
+      fetchWidowerDataSuccess({
+        profile: finalProfileList,
+        isPaginationRequired: data.data.length === PAGE_SIZE,
+        pageNumber: payload.page,
+      }),
+    );
   } else {
+    yield put(fetchWidowerDataFail({}));
     showMessage({
       message: 'Ops, something went wrong',
       type: 'danger',
     });
-    fetchWidowerDataFail(problem);
   }
 }
